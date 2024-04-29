9b13e329d5dbeeb77f95c267f005314cfe918e00
coderroot2 <udayraut128@gmail.com>
2024-04-29 23:25:27 +0530

    add comment code in python

---
+++ Python Tutorials/Comments/Comment.py
@@ -0,0 +1,7 @@
+# In this section we learn about python comments
+
+#  Single line comment starts with a hash (#)
+print("Hello World")   # This is a single line comment. Python interpreter ignores anything after the '#' symbol until it encounters a and goes until the end of the line.
+
+# OUTPUT
+# Hello World 
