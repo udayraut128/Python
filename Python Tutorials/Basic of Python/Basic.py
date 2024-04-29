f75ed890e855dd15c2ae44e65dd27fa181ce866b
coderroot2 <udayraut128@gmail.com>
2024-04-29 23:22:32 +0530

    add sum  code in python

--- Python Tutorials/Basic of Python/Basic.py
+++ Python Tutorials/Basic of Python/Basic.py
@@ -1,2 +1,12 @@
 # our first code of Python
 print("Hello world!")
+
+
+# Add of two contstant variables(numbers)
+a=4
+b=6
+sum = a + b
+print ("The sum of", a, "and", b,"is", sum)
+
+# OUTPUT
+# The sum of 4 and 6 is 10
