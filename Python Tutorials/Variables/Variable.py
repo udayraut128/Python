78869ec8cf3d03eaf0c2a6580bf183df3b7f56e5
coderroot2 <udayraut128@gmail.com>
2024-04-30 22:51:03 +0530

    add variable in python

---
+++ Python Tutorials/Variables/Variable.py
@@ -0,0 +1,15 @@
+# Variable Declaration and Assignment: In Python, you can declare a variable and assign a value to it in a single line. For example:
+x = 10
+name = "John"
+
+
+
+x = 10          # x is an integer
+name = "John"   # name is a string
+
+# Multiple Assignment: Python allows you to assign multiple values to multiple variables in a single line. For example:
+x, y, z = 10, 20, 30
+
+# Reassignment: You can change the value of a variable by assigning it a new value. For example:
+x = 10
+x = 20   # Reassigning x to a new value
