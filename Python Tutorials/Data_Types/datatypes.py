391c1599a22e9430c22aa08de72ae1efead342c1
coderroot2 <udayraut128@gmail.com>
2024-04-30 22:53:22 +0530

    data types in python

---
+++ Python Tutorials/Data_Types/datatypes.py
@@ -0,0 +1,11 @@
+
+
+
+x = 5         # x is an integer
+name = "John" # name is a string
+my_list = [1, 2, 3]  # my_list is a list
+
+# You can use the type() function to check the data type of a variable:
+print(type(x))      # <class 'int'>
+print(type(name))   # <class 'str'>
+print(type(my_list)) # <class 'list'>
