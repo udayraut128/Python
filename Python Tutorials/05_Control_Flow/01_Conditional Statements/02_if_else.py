3bc6c342050d2452bdb281028efbe7ec9e0feeb9
coderroot2 <udayraut128@gmail.com>
2024-05-10 11:20:11 +0530

    add ifelse code

---
+++ Python Tutorials/05_Control_Flow/01_Conditional Statements/02_if_else.py
@@ -0,0 +1,34 @@
+# 1 example
+
+# Define a variable
+x = 10
+
+# Check if x is greater than 5
+if x > 5:
+    # If condition is true, print a message
+    print("x is greater than 5")
+else:
+    # If condition is false, print a different message
+    print("x is not greater than 5")
+
+# Output
+# x is not greater than 5
+
+# 2 example
+
+# Define two variables
+a = 5
+b = 10
+
+# Check if a is less than b
+if a < b:
+    # If condition is true, print a message
+    print("a is less than b")
+else:
+    # If condition is false, print a different message
+    print("a is greater than b")
+
+
+# Output
+# a is less than b
+
