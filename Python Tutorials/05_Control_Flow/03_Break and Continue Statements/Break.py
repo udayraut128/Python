975a3131526aa4bf507b3d8929e47208db4d4bf8
coderroot2 <udayraut128@gmail.com>
2024-05-11 21:51:29 +0530

    add break code of python

---
+++ Python Tutorials/05_Control_Flow/03_Break and Continue Statements/Break.py
@@ -0,0 +1,14 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Iterate over the list
+for number in numbers:
+    # Check if the number is greater than 5
+    if number > 5:
+        # If it is, print the number and exit the loop
+        print("Found a number greater than 5:", number)
+        break
+
+
+#output
+# Found a number greater than 5: 6
