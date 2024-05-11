c549f057786f0fd1a5eb083df0bec6c4887652d9
coderroot2 <udayraut128@gmail.com>
2024-05-11 21:56:26 +0530

    add another example of break

--- Python Tutorials/05_Control_Flow/03_Break and Continue Statements/Break.py
+++ Python Tutorials/05_Control_Flow/03_Break and Continue Statements/Break.py
@@ -12,3 +12,20 @@ for number in numbers:
 
 #output
 # Found a number greater than 5: 6
+
+# another example
+
+# Define a list of numbers
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# Iterate over the list
+for number in numbers:
+    # Check if the number is divisible by 30
+    if number % 30 == 0:
+        # If it is, print the number and exit the loop
+        print("Found a number divisible by 30:", number)
+        break
+
+
+#output
+# Found a number divisible by 30: 30
