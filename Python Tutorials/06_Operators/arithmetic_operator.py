219772d44e7b4012ef24a261189146ce472850c2
coderroot2 <udayraut128@gmail.com>
2024-05-12 20:26:46 +0530

    add arithmetic operator

---
+++ Python Tutorials/06_Operators/arithmetic_operator.py
@@ -0,0 +1,38 @@
+# Addition
+result_addition = 10 + 5
+print("Addition:", result_addition)
+
+# Subtraction
+result_subtraction = 10 - 5
+print("Subtraction:", result_subtraction)
+
+# Multiplication
+result_multiplication = 10 * 5
+print("Multiplication:", result_multiplication)
+
+# Division
+result_division = 10 / 5
+print("Division:", result_division)
+
+# Floor Division
+result_floor_division = 10 // 3
+print("Floor Division:", result_floor_division)
+
+# Modulus
+result_modulus = 10 % 3
+print("Modulus:", result_modulus)
+
+# Exponentiation
+result_exponentiation = 2 ** 3
+print("Exponentiation:", result_exponentiation)
+
+
+
+#output
+# Addition: 15
+# Subtraction: 5
+# Multiplication: 50
+# Division: 2.0
+# Floor Division: 3
+# Modulus: 1
+# Exponentiation: 8
