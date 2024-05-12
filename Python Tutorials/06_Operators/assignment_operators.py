e68dc058a8888cb98dde416deb22bfae88ac6c7d
coderroot2 <udayraut128@gmail.com>
2024-05-12 20:31:33 +0530

    add assignment operator

---
+++ Python Tutorials/06_Operators/assignment_operators.py
@@ -0,0 +1,17 @@
+x = 10
+x += 5   # Equivalent to: x = x + 5
+print("Addition assignment:", x)
+
+y = 20
+y -= 5   # Equivalent to: y = y - 5
+print("Subtraction assignment:", y)
+
+z = 10
+z *= 2   # Equivalent to: z = z * 2
+print("Multiplication assignment:", z)
+
+
+#output
+# Addition assignment: 15
+# Subtraction assignment: 15
+# Multiplication assignment: 20
