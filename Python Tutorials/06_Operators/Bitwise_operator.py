d796a508b64f71561ae7fcd0df5fbf292115af1e
coderroot2 <udayraut128@gmail.com>
2024-05-12 20:33:57 +0530

    add bitwise operator

---
+++ Python Tutorials/06_Operators/Bitwise_operator.py
@@ -0,0 +1,32 @@
+# Bitwise AND
+result_and = 10 & 5
+print("Bitwise AND:", result_and)
+
+# Bitwise OR
+result_or = 10 | 5
+print("Bitwise OR:", result_or)
+
+# Bitwise XOR
+result_xor = 10 ^ 5
+print("Bitwise XOR:", result_xor)
+
+# Bitwise NOT
+result_not = ~10
+print("Bitwise NOT:", result_not)
+
+# Left shift
+result_left_shift = 10 << 2
+print("Left shift:", result_left_shift)
+
+# Right shift
+result_right_shift = 10 >> 2
+print("Right shift:", result_right_shift)
+
+
+#output
+# Bitwise AND: 0
+# Bitwise OR: 15
+# Bitwise XOR: 15
+# Bitwise NOT: -11
+# Left shift: 40
+# Right shift: 2
