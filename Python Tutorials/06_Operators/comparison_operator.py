8f34ac46b6c8d28d1b818bc9688bf977072f8b70
coderroot2 <udayraut128@gmail.com>
2024-05-12 20:28:04 +0530

    add comparison operator

---
+++ Python Tutorials/06_Operators/comparison_operator.py
@@ -0,0 +1,26 @@
+# Equal to
+print(5 == 5)  # True
+
+# Not equal to
+print(5 != 5)  # False
+
+# Greater than
+print(10 > 5)  # True
+
+# Less than
+print(10 < 5)  # False
+
+# Greater than or equal to
+print(10 >= 5)  # True
+
+# Less than or equal to
+print(10 <= 5)  # False
+
+
+#output
+# True
+# False
+# True
+# False
+# True
+# False
