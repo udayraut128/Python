8e9d43fe1f8b003c39794081c50ae91d56ff8150
coderroot2 <udayraut128@gmail.com>
2024-05-13 16:26:06 +0530

    add second ex of list

---
+++ Python Tutorials/09_Tuples/02_Tuple.py
@@ -0,0 +1,27 @@
+# Creating a tuple of coordinates
+point = (3, 7)
+
+# Accessing elements
+print("X coordinate:", point[0])
+print("Y coordinate:", point[1])
+
+# Tuple unpacking
+x, y = point
+print("Unpacked coordinates:", x, y)
+
+# Creating a tuple of mixed data types
+person = ("John", 30, "New York")
+
+# Accessing elements
+print("Name:", person[0])
+print("Age:", person[1])
+print("City:", person[2])
+
+
+#output
+# X coordinate: 3
+# Y coordinate: 7
+# Unpacked coordinates: 3 7
+# Name: John
+# Age: 30
+# City: New York
