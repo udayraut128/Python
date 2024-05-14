340e9a28a912ba1d35a1eeb064d9eaf447f81d38
coderroot2 <udayraut128@gmail.com>
2024-05-14 15:19:45 +0530

    add function with parameter and return type

--- Python Tutorials/11_Functions/02_add_of_two_num.py
+++
@@ -1,9 +0,0 @@
-# Function definition
-def add_numbers(x, y):
-    return x + y
-
-# Function call
-result = add_numbers(3, 5)
-print(result)  
-
-# Output: 8
---
+++ Python Tutorials/11_Functions/03_Function.py
@@ -0,0 +1,10 @@
+# 3.Function with Parameters and Return Value:
+
+def add_numbers(x, y):
+    return x + y
+
+result = add_numbers(3, 5)
+print("Sum:", result)
+
+#output
+# Sum: 8
