f8809fda31b61912d4f3136b6b8ebae776e2e4ee
coderroot2 <udayraut128@gmail.com>
2024-05-14 15:22:01 +0530

    add function with default parameters

---
+++ Python Tutorials/11_Functions/04_Function.py
@@ -0,0 +1,12 @@
+#Function with Default Parameters:
+
+
+def greet(name="world"):
+    print("Hello, " + name + "!")
+
+greet()  # Output: Hello, world!
+greet("Alice")  # Output: Hello, Alice!
+
+#output
+# Hello, world!
+# Hello, Alice!
