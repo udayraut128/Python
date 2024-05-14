97f93e691fbe5245dd0d17c14109a719f23789e5
coderroot2 <udayraut128@gmail.com>
2024-05-14 15:13:25 +0530

    add function

---
+++ Python Tutorials/11_Functions/01_Function.py
@@ -0,0 +1,14 @@
+# A function in Python is a reusable block of code that performs a specific task. It consists of a function header with a name and optional parameters, followed by a function body where the task is defined. Functions can optionally return a value using the return statement.
+
+
+
+# Define a function
+def greet(name):
+    return "Hello, " + name + "!"
+
+# Call the function
+message = greet("Alice")
+print(message)
+
+#output
+# Hello, Alice!
---
+++ Python Tutorials/11_Functions/02_add_of_two_num.py
@@ -0,0 +1,9 @@
+# Function definition
+def add_numbers(x, y):
+    return x + y
+
+# Function call
+result = add_numbers(3, 5)
+print(result)  
+
+# Output: 8
