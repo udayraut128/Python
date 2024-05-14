93161b36c4757003d196f5ac6df53f49106b8559
coderroot2 <udayraut128@gmail.com>
2024-05-14 15:31:08 +0530

    add Function with Keyword Arguments:

---
+++ Python Tutorials/11_Functions/06_Function.py
@@ -0,0 +1,7 @@
+# Function with Keyword Arguments:
+
+def greet(name, message):
+    print("Hello, " + name + "! " + message)
+
+greet(name="Alice", message="How are you?")
+# Output: Hello, Alice! How are you?
