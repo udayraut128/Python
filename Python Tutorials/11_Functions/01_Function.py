02a3ef564cf2ca1f60768ad025407cf98d20893b
coderroot2 <udayraut128@gmail.com>
2024-05-14 15:17:07 +0530

    add function with parameter

--- Python Tutorials/11_Functions/01_Function.py
+++ Python Tutorials/11_Functions/01_Function.py
@@ -1,6 +1,6 @@
 # A function in Python is a reusable block of code that performs a specific task. It consists of a function header with a name and optional parameters, followed by a function body where the task is defined. Functions can optionally return a value using the return statement.
 
-
+# 1.Function without Parameters and Return Value:
 def greet():
     print("Hello, world!")
 
---
+++ Python Tutorials/11_Functions/02_function.py
@@ -0,0 +1,8 @@
+# 2.Function with Parameters but without Return Value:
+
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("Alice")
+
+# Hello, Alice!
