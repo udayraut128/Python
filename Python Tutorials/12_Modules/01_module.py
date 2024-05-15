d9dfa7deb8950b3f57b436ac3f5486275ec5882f
coderroot2 <udayraut128@gmail.com>
2024-05-15 17:36:49 +0530

    add module 1 code

---
+++ Python Tutorials/12_Modules/01_module.py
@@ -0,0 +1,22 @@
+#  a module in Python is a file containing reusable Python code. Modules help organize and manage code by providing a way to group related functionality together. They can be imported into other Python scripts using the import statement, allowing you to access the variables, functions, and classes defined within the module. Modules are essential for code reuse, organization, and maintaining a clean project structure.
+
+
+
+# Importing the math module
+import math
+
+# Using functions from the math module
+print(math.sqrt(25))  # Output: 5.0
+print(math.pi)        # Output: 3.141592653589793
+
+# Importing specific functions from a module
+from math import factorial
+
+# Using the factorial function
+print(factorial(5))   # Output: 120
+
+
+#output
+# 5.0
+# 3.141592653589793
+# 120
