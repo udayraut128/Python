16c0c36191ea4ffd58c63b30d24d7a7547e272ac
coderroot2 <udayraut128@gmail.com>
2024-05-15 17:40:50 +0530

    add calulator module

---
+++ Python Tutorials/12_Modules/02_Module.py
@@ -0,0 +1,22 @@
+# main.py
+#import form calucator.py file
+import calculator
+
+result_add = calculator.add(5, 3)
+print("Addition Result:", result_add)  # Output: 8
+
+result_subtract = calculator.subtract(10, 4)
+print("Subtraction Result:", result_subtract)  # Output: 6
+
+result_multiply = calculator.multiply(6, 7)
+print("Multiplication Result:", result_multiply)  # Output: 42
+
+result_divide = calculator.divide(20, 4)
+print("Division Result:", result_divide)  # Output: 5.0
+
+
+#output
+# Addition Result: 8
+# Subtraction Result: 6
+# Multiplication Result: 42
+# Division Result: 5.0
---
+++ Python Tutorials/12_Modules/calculator.py
@@ -0,0 +1,13 @@
+# calculator.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
