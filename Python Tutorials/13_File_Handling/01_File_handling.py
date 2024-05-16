7936d481e3ce23e3a87b30b3c2ac0989e2f75a65
coderroot2 <udayraut128@gmail.com>
2024-05-16 16:22:48 +0530

    Delete 01_File_handling.py
    a

--- Python Tutorials/13_File_Handling/01_File_handling.py
+++
@@ -1,2 +0,0 @@
-# File handling in Python refers to the manipulation of files on the filesystem using Python programming language. It allows you to read data from files, write data to files, and perform various operations such as creating, deleting, and modifying files.
-
