1f60a2017b0ed03511408ece67eb36d64cd21d24
coderroot2 <udayraut128@gmail.com>
2024-05-20 18:34:43 +0530

    add custom context manager

---
+++ Python Tutorials/17_Context_Managers/05_Custom_context_manager/custom.py
@@ -0,0 +1,15 @@
+class FileHandler:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file:
+            self.file.close()
+
+with FileHandler('example.txt', 'w') as file:
+    file.write('Hello, context manager!')
