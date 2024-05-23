9f64333a96dbd74f1ddf0a85737583db091a5949
coderroot2 <udayraut128@gmail.com>
2024-05-23 19:24:26 +0530

    add 2 multithreading

---
+++ Python Tutorials/19_Multithreading/02_Multithreading.py
@@ -0,0 +1,24 @@
+import threading
+
+shared_resource = 0
+lock = threading.Lock()
+
+def update_shared_resource():
+    global shared_resource
+    with lock:
+        shared_resource += 1
+
+threads = []
+
+for _ in range(10):
+    thread = threading.Thread(target=update_shared_resource)
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+print("Shared resource:", shared_resource)
+
+#output
+# Shared resource: 10
