45092d608bd4815ecdfe49f70e5422bd3ad36977
Uday Raut <rautuday75@gmail.com>
2025-02-08 15:29:07 +0530

    add ass1

--- Python-Nptel/w3/ass1.py
+++ Python-Nptel/w3/ass1.py
@@ -1,1 +1,8 @@
-print("Hello")
+numbers = list(map(int, input().split()))
+unique_numbers = list(set(numbers))  # Remove duplicates
+unique_numbers.sort()  # Sort the list in ascending order
+
+if len(unique_numbers) < 2:
+    print("No second smallest number")
+else:
+    print(unique_numbers[1])  # Print the second smallest number
