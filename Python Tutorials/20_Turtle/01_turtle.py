ec718d11d58937fedfbc8298dae8d9e2a309e965
Uday Raut <rautuday75@gmail.com>
2025-05-04 23:43:04 +0530

    add turtle

---
+++ Python Tutorials/20_Turtle/01_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+t=turtle.Turtle()
+
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+
+turtle.done()
+
+ 
---
+++ Python Tutorials/20_Turtle/02_turtle.py
@@ -0,0 +1,17 @@
+#rectangle
+import turtle
+
+t=turtle.Turtle()
+
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+turtle.done()
+
+ 
